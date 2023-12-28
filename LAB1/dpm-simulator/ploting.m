a6ceb20c9750ab1ce166fe36314fd68f590aaf53
Edoardo Manfredi <edoardom59@tutanota.com>
2023-12-28 11:12:23 +0100

    feat: add total time plot

--- LAB1/dpm-simulator/ploting.m
+++ LAB1/dpm-simulator/ploting.m
@@ -1,15 +1,18 @@
-dataFilePath = 'C:\Users\marco\OneDrive\Documenti\emi-labs\LAB1\dpm-simulator\results\result1_1000_10.txt';
+format long
+fileName = 'result2_1000_0.1.txt';
+dataFilePath = [ 'results', filesep, fileName ];
 data = readtable(dataFilePath, 'HeaderLines', 0);
 
-timeout = data(1:2:end, 1);
-totalEnergy = data(2:2:end, 11);
-timeout = table2array(timeout);
-totalEnergy = table2array(totalEnergy);
-
 figure;
-%plot(timeout, totalEnergy, 'o-', 'LineWidth', 2, 'MarkerSize', 15, 'MarkerFaceColor', 'auto');
-plot(timeout, totalEnergy, '.','MarkerSize', 15);
+plot(data.timeout, data.total_energy, '.','MarkerSize', 12, 'MarkerFaceColor', 'auto');
 title('Timeout-Total Energy');
 xlabel('Timeout');
 ylabel('Total Energy');
+grid on;
+ 
+figure;
+plot(data.timeout, data.total_time, '.', 'MarkerSize', 12);
+title('Timeout-Total Time');
+xlabel('Timeout');
+ylabel('Total Time');
 grid on;
